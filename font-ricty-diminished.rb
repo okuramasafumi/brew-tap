6f860ce14c2ccd3e073a5f84c8c812e13093c3bd
Okura Masafumi <masafumi.o1988@gmail.com>
2015-01-10 12:33:06 +0900

    Add font-ricty-diminished.rb

---
+++ font-ricty-diminished.rb
@@ -0,0 +1,17 @@
+cask :v1 => 'font-ricty-diminished' do
+  version '3.2.4'
+  sha256 :no_check
+
+  url 'https://github.com/yascentur/RictyDiminished/archive/3.2.4.zip'
+  homepage 'https://github.com/yascentur/RictyDiminished'
+  license :oss
+
+  font 'RictyDiminished-Bold.ttf'
+  font 'RictyDiminished-BoldOblique.ttf'
+  font 'RictyDiminished-Oblique.ttf'
+  font 'RictyDiminished-Regular.ttf'
+  font 'RictyDiminishedDiscord-Bold.ttf'
+  font 'RictyDiminishedDiscord-BoldOblique.ttf'
+  font 'RictyDiminishedDiscord-Oblique.ttf'
+  font 'RictyDiminishedDiscord-Regular.ttf'
+end
